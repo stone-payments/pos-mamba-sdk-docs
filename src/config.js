1c818a87dccc7c1d9f952d3d42da9a93dd9b6eef
jaikme <jaime.mqu@gmail.com>
2022-03-15 15:46:39 -0300

    fix: 🐛 ca assert

--- src/config.js
+++ src/config.js
@@ -1,25 +1,33 @@
 const path = require('path')
 
+const cas = process.env.HTTPS_CA_PATH_LIST
+
+const certs = {
+  key: process.env.HTTPS_KEY_PATH
+    ? process.env.HTTPS_KEY_PATH
+    : path.join(process.cwd(), 'localhost.key'),
+  cert: process.env.HTTPS_CERT_PATH
+    ? process.env.HTTPS_CERT_PATH
+    : path.join(process.cwd(), 'localhost.cert'),
+}
+
+if (process.env.HTTPS_CA_PATH_LIST) {
+  try {
+    certs.ca = String(process.env.HTTPS_CA_PATH_LIST)
+      .split(',')
+      .map((f) => String(f).trim())
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 module.exports = {
   credentials: {
     dev: {
-      key: process.env.HTTPS_KEY_PATH
-        ? process.env.HTTPS_KEY_PATH
-        : path.join(process.cwd(), 'localhost.key'),
-      cert: process.env.HTTPS_CERT_PATH
-        ? process.env.HTTPS_CERT_PATH
-        : path.join(process.cwd(), 'localhost.cert'),
+      ...certs,
       requestCert: false,
       rejectUnauthorized: false,
     },
-    prod: {
-      key: process.env.HTTPS_KEY_PATH
-        ? process.env.HTTPS_KEY_PATH
-        : path.join(process.cwd(), 'localhost.key'),
-      cert: process.env.HTTPS_CERT_PATH
-        ? process.env.HTTPS_CERT_PATH
-        : path.join(process.cwd(), 'localhost.cert'),
-      ca: JSON.parse(process.env.HTTPS_CA_PATH_LIST || '[]'),
-    },
+    prod: certs,
   },
 }
